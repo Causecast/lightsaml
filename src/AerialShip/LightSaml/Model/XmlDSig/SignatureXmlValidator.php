ee20836e295e7cd4f56c6e5e2eeaf2140f4b9f17
Alexander Kaluga <alex@causecast.org>
2014-10-31 16:18:10 -0700

    fixed namespace recognition for assertion signature

--- src/AerialShip/LightSaml/Model/XmlDSig/SignatureXmlValidator.php
+++ src/AerialShip/LightSaml/Model/XmlDSig/SignatureXmlValidator.php
@@ -171,6 +171,8 @@ class SignatureXmlValidator extends Signature implements LoadFromXmlInterface, S
     private function getAlgorithm()
     {
         $xpath = new \DOMXPath($this->signature->sigNode instanceof \DOMDocument ? $this->signature->sigNode : $this->signature->sigNode->ownerDocument);
+        $xpath->registerNamespace('ds', \XMLSecurityDSig::XMLDSIGNS);
+
         $list = $xpath->query('./ds:SignedInfo/ds:SignatureMethod', $this->signature->sigNode);
         if ($list->length == 0) {
             throw new InvalidXmlException('Missing SignatureMethod element');
